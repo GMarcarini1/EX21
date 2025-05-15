1ed78423dd431630d0f81f2274355fd371b28dbf
Rafael-ct <rafael.202411103@unilasalle.edu.br>
2025-05-15 05:48:29 -0300

    Começo da implementação da pilha e adição da tela de comandos.

--- main.c
+++ main.c
@@ -6,12 +6,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 100
 
 struct ALUNO {
   int matricula;
   double g1, g2, media;
 
 };
+
+int pilha[TAM], topo;
+
+void inicia() {
+  topo = -1
+}
+
+int empty(){
+  if (topo == -1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+int full(){
+  if (topo == (TAM -1))
+  {
+    return 1;
+  }
+  return 0;
+}
+
+void pop(){
+  int vazio;
+  vazio = empty();
+  if(vazio == 1){
+    printf("============================");
+    printf("\nEscola sem alunos.\n");
+  } 
+  topo --;
+}
+
+void push(int matricula) {
+  int retorno;
+  retorno = full();
+   if (retorno == 1){
+     printf("\nA escola está cheia, não pode mais aceitar alunos.\n");
+   }
+   else{
+     topo++;
+      pilha[topo] = matricula;
+   } 
+  }
+
+  void imp()
+  {
+    int i;
+    printf("\n============ PILHA ============ \n");
+    for (i = 0 ; i <= topo; i++){
+      printf("%d\n", pilha[i]);
+    }
+  }
 
 int preenchendo_alunos(struct ALUNO *alunos, int qnt){
   
@@ -31,26 +86,58 @@ int preenchendo_alunos(struct ALUNO *alunos, int qnt){
 
     alunos[i].media = (alunos[i].g1 + alunos[i].g2) / 2.0;
     
+    push(alunos[i].matricula);
   }
   
-  return 200;
+  return 0;
   
 };
 
 int main(){
   int qnt;
   struct ALUNO *alunos;
+  int escolha;
+  inicia();
 
-  printf("Quantos\n");
-  scanf("%i", &qnt);
+  do{
+    printf("=======tela de comandos========\n");
+    printf("caso queira adicionar alunos a pilha Digite (1).\n");
+    printf("caso queira remover alunos da pilha Digite (2).\n");
+    printf("caso queira sair Digite (3).\n");
+    printf("===============================\n");
+  
+    printf("O que você deseja fazer? ");
+    scanf("%i", &escolha);
 
-  alunos = malloc(qnt*sizeof(struct ALUNO));
-  if (alunos == NULL) {
-    //erro nao alocou
-    return 10;
-  }
+    if(escolha == 1 ){  
+    printf("Quantos\n");
+    scanf("%i", &qnt);
 
-  preenchendo_alunos(alunos, qnt);
-
-  free(alunos);
+    alunos = malloc(qnt*sizeof(struct ALUNO));
+    if (alunos == NULL) {
+      //erro nao alocou
+      return 10;
+    }
+    preenchendo_alunos(alunos, qnt);
+    imp();
+    free(alunos);
+    }
+    else if(escolha == 2){
+      pop();
+      imp();
+    }
+    else if(escolha == 3){
+      printf("Removendo todos os alunos.\n");
+      do{
+        pop();
+      } while(topo != -1);
+      imp();
+      
+      printf("Programa encerrando com sucesso.\n");
+      printf("===============================\n");
+    }
+    else{
+      printf("Comando inválido, digite novamente.\n");
+    }
+  } while(escolha != 3);
 }
