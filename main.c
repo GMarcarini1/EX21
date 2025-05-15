be7a5abbc3ebaf392e4081ae063b53456ef153b6
GMarcarini1 <giovanni.202410131@unilasalle.edu.br>
2025-05-15 14:38:36 -0300

    Impletação das funções PUSH, POP, EMPTY entre outras
    Funções adicionadas
    inicia
    verifica_vazia
    verifica_cheia
    push
    pop
    Melhoria na estruturação dos dados da PILHA e ALUNO

--- main.c
+++ main.c
@@ -1,143 +1,129 @@
-// Integrantes do grupo
-// Giovanni Marcarini Silveira | 202410131 | giovanni.202410131@unilasalle.edu.br
-// Arthur Contri Gasperin | 202410137 | arthur.202410137@unilasalle.edu.br
-// Rafael Decarli Vargas | 202411103 | rafael.202411103@unilasalle.edu.br
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM 100
 
-struct ALUNO {
+typedef struct{
+  //Estrutura do armazenamento de dados dos alunos
+
   int matricula;
   double g1, g2, media;
 
-};
+} ALUNO;
 
-int pilha[TAM], topo;
+typedef struct{
+  // Estrutura da Pilha
 
-void inicia() {
-  topo = -1
+  ALUNO *q;
+  int topo;
+  int qntmax;
+} PILHA;
+
+int inicia(PILHA *p, int qnt) {
+  // Função para inicializar a pilha, armazena a quantidade máxima da pilha, e reseta o topo
+
+  p->topo = -1;
+  p->qntmax = qnt;
+  return 200;
 }
 
-int empty(){
-  if (topo == -1){
-    return 1;
-  }
-  else{
-    return 0;
-  }
+int verifica_vazia(PILHA *p){
+  //Verifica se a pilha esta vazia comparando o topo com o valor inicial -1
+  return (p->topo == -1);
 }
 
-int full(){
-  if (topo == (TAM -1))
-  {
+int verifica_cheia(PILHA *p){
+  // Verifica se a pilha esta cheia comparando o topo com o valor de qntmax
+  return (p->topo == p->qntmax-1);
+}
+
+int push(PILHA *p, ALUNO novo){
+  //Verifica se a pilha está cheia e depois adiciona o aluno no topo da pilha
+  if (verifica_cheia(p)){
+    //erro pilha cheia
     return 1;
   }
+
+  p->topo++;
+  p->q[p->topo] = novo;
+
   return 0;
 }
 
-void pop(){
-  int vazio;
-  vazio = empty();
-  if(vazio == 1){
-    printf("============================");
-    printf("\nEscola sem alunos.\n");
-  } 
-  topo --;
+int pop(PILHA *p){
+  //Verifica se a pilha está vazia e depois remove o aluno no topo da pilha
+  if(verifica_vazia(p)){
+    return 1;
+  }
+
+  p->topo--;
+  return 0;
 }
 
-void push(int matricula) {
-  int retorno;
-  retorno = full();
-   if (retorno == 1){
-     printf("\nA escola está cheia, não pode mais aceitar alunos.\n");
-   }
-   else{
-     topo++;
-      pilha[topo] = matricula;
-   } 
-  }
-
-  void imp()
-  {
-    int i;
-    printf("\n============ PILHA ============ \n");
-    for (i = 0 ; i <= topo; i++){
-      printf("%d\n", pilha[i]);
-    }
-  }
-
-int preenchendo_alunos(struct ALUNO *alunos, int qnt){
-  
-  int i;
-  
-  for (i=0; i< qnt; i++) {
-    printf("======== ALUNO %d ========\n", i+1);
-
-    printf("Matrícula: ");
-    scanf("%d", &alunos[i].matricula);
-
-    printf("Nota G1: ");
-    scanf("%lf", &alunos[i].g1);
-
-    printf("Nota G2: ");
-    scanf("%lf", &alunos[i].g2);
-
-    alunos[i].media = (alunos[i].g1 + alunos[i].g2) / 2.0;
-    
-    push(alunos[i].matricula);
-  }
-  
-  return 0;
-  
-};
 
 int main(){
-  int qnt;
-  struct ALUNO *alunos;
-  int escolha;
-  inicia();
+  int qnt, i;
+  PILHA *p;
+  char letra;
 
-  do{
-    printf("=======tela de comandos========\n");
-    printf("caso queira adicionar alunos a pilha Digite (1).\n");
-    printf("caso queira remover alunos da pilha Digite (2).\n");
-    printf("caso queira sair Digite (3).\n");
-    printf("===============================\n");
+  printf("Olá!\n");
+  printf("Quantos alunos deseja adicionar?");
+  scanf("%d", &qnt);
+
+  p = malloc(sizeof(PILHA));
+  if (p == NULL) {
+    //erro não alocou
+    return 1;
+  }
+
+  p->q= malloc(qnt*sizeof(ALUNO));
+  if (p->q == NULL) {
+    //erro não alocou
+    return 1;
+  }
+
+  //Iniciza a pilha
+  inicia(p, qnt);
+
+  //Cadastro de Alunos
+  printf("CADASTRANDO ALUNOS\n");
+
+  for (i=0; i<qnt; i++){
+    ALUNO novo;
+
+    printf("ALUNO %d de %d\n", i+1, qnt);
+
+    printf("Matrícula:");
+    scanf("%d", &novo.matricula);
+
+    printf("Nota G1:");
+    scanf("%lf", &novo.g1);
+
+    printf("Nota g2:");
+    scanf("%lf", &novo.g2);
+
+    novo.media = (novo.g1 + novo.g2)/2;
+
+    push(p, novo);
+    printf("Aluno %d Adicionado com sucesso!\n", novo.matricula);
+
+    printf("Você deseja remover o último aluno adicionado? (s/n)");
+    scanf(" %c", &letra);
+
+    if (letra == 's'){
+      pop(p);
+      i--;
+
+    }
+
+  }
+
+  printf("Os alunos abaixo foram adicionados com sucesso\n");
+  for(i=0; i<qnt; i++){
+    printf("%d\n", p->q[i].matricula);
+  }
+
+  printf("Encerrando o Programa...");
   
-    printf("O que você deseja fazer? ");
-    scanf("%i", &escolha);
+  free(p->q);
+  free(p);
 
-    if(escolha == 1 ){  
-    printf("Quantos\n");
-    scanf("%i", &qnt);
-
-    alunos = malloc(qnt*sizeof(struct ALUNO));
-    if (alunos == NULL) {
-      //erro nao alocou
-      return 10;
-    }
-    preenchendo_alunos(alunos, qnt);
-    imp();
-    free(alunos);
-    }
-    else if(escolha == 2){
-      pop();
-      imp();
-    }
-    else if(escolha == 3){
-      printf("Removendo todos os alunos.\n");
-      do{
-        pop();
-      } while(topo != -1);
-      imp();
-      
-      printf("Programa encerrando com sucesso.\n");
-      printf("===============================\n");
-    }
-    else{
-      printf("Comando inválido, digite novamente.\n");
-    }
-  } while(escolha != 3);
 }
